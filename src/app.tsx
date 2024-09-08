137ca72ef376f8abe151914f9ad7182bcf9600fe
TinyBlueSapling <142849583+TinyBlueSapling@users.noreply.github.com>
2024-09-08 18:44:21 -0300

    Initial layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,24 @@
-import { createSignal } from 'solid-js'
+import { A } from '@solidjs/router';
+import ionicLogo from './assets/ionic.png';
 
-function App() {
+function App(props: any) {
   return (
-    <h3>WIP</h3>
+    <div class="min-w-screen min-h-screen bg-[#FEFBFE]">
+        <nav class="flex flex-row items-center p-4 border-b border-black">
+          <div class="flex flex-row items-center mr-8">
+            <img src={ionicLogo} alt="Ionic logo" class="max-h-20 mr-4" />
+            <h1 class="font-semibold text-4xl">Ionic</h1>
+          </div>
+          <ul class="flex flex-row w-full space-x-4 text-xl items-center">
+            <li><A class="hover:underline" activeClass="underline" end href="/">Home</A></li>
+            <li><a class="hover:underline" target="_blank" href={import.meta.env.VITE_DISCORD_INVITE_URL}>Invite</a></li>
+            <li class="!ml-auto"><A href="/dashboard" class="bg-[#6C96DA] hover:bg-[#3066BE] text-white p-4 rounded-xl" activeClass="bg-[#3066BE]">Dashboard</A></li>
+          </ul>
+        </nav>
+        <div class="p-4">
+          {props.children}
+        </div>
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { lazy } from 'solid-js';
+import { render } from 'solid-js/web';
+import { Router, Route } from "@solidjs/router";
 
-import './index.css'
-import App from './App'
+import './index.css';
 
-const root = document.getElementById('root')
+import App from './App';
+const DashboardHome = lazy(() => import("./pages/DashboardHome"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Leaderboard = lazy(() => import("./pages/Leaderboard"));
+const Home = lazy(() => import("./pages/Home"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
-render(() => <App />, root!)
+const root = document.getElementById('root');
+
+render(() => (<Router root={App}>
+    <Route path="/dashboard" component={DashboardHome} />
+    <Route path="/dashboard/:guildId" component={Dashboard} />
+    <Route path="/leaderboard/:guildId" component={Leaderboard} />
+    <Route path="/" component={Home} />
+    <Route path="*404" component={NotFound} />
+</Router>), root!);
