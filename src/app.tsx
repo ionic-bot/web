4d7e30eea1c3460936296ff34d58b2311256f74a
TinyBlueSapling <142849583+TinyBlueSapling@users.noreply.github.com>
2024-09-20 20:21:03 -0300

    Implement leaderboard.

--- src/app.tsx
+++ src/app.tsx
@@ -4,15 +4,15 @@ import ionicLogo from './assets/ionic.png';
 function App(props: any) {
   return (
     <div class="min-w-screen min-h-screen bg-[#FEFBFE]">
-        <nav class="flex flex-row items-center p-4 border-b border-black">
+        <nav class="flex flex-row items-center p-4 bg-white border-b border-[#EF8354]">
           <div class="flex flex-row items-center mr-8">
-            <img src={ionicLogo} alt="Ionic logo" class="max-h-20 mr-4" />
+            <img src={ionicLogo} alt="Ionic logo" class="max-h-12 mr-4" />
             <h1 class="font-semibold text-4xl">Ionic</h1>
           </div>
           <ul class="flex flex-row w-full space-x-4 text-xl items-center">
             <li><A class="hover:underline" activeClass="underline" end href="/">Home</A></li>
             <li><a class="hover:underline" target="_blank" href={import.meta.env.VITE_DISCORD_INVITE_URL}>Invite</a></li>
-            <li class="!ml-auto"><A href="/dashboard" class="bg-[#6C96DA] hover:bg-[#3066BE] text-white p-4 rounded-xl" activeClass="bg-[#3066BE]">Dashboard</A></li>
+            <li class="!ml-auto"><A href="/dashboard" class="bg-[#3066BE] hover:bg-[#2957A3] active:bg-[#224886] text-white px-6 py-4 rounded-full" activeClass="bg-[#224886]">Dashboard</A></li>
           </ul>
         </nav>
         <div class="p-4">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <p>
-      Home
+      Welcome to Ionic!
     </p>
   )
 }
